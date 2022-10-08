6e8578f3eabda969eecb8672dbaf9da586ab0f95
Ricardo <ricardocb007@gmail.com>
2022-10-08 14:11:54 -0500

    Se agrego un grid para la visualizacion de articulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './encabezado/encabezado.component';
 import { IonicModule } from '@ionic/angular';
+import { ArticulosComponent } from './articulos/articulos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EncabezadoComponent
+    EncabezadoComponent,                                 
+    ArticulosComponent
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot()
+    AppRoutingModule,
+    IonicModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent], 
